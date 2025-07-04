3ec104a5e4e5d061f5067c8feb1af384513f4845
Himasri <himasrikatam@gmail.com>
2025-07-04 10:47:02 +0530

    removed mdb

--- crop_flask.py
+++ crop_flask.py
@@ -1,13 +1,13 @@
 # c:\Users\HP\Desktop\crop_recommendation\.venv\Scripts\python.exe -m pip install flask-pymongo
 import pickle
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from flask import Flask
 from flask import request, render_template
 
 app = Flask(__name__)
 model, le = pickle.load(open('model_pickle', 'rb'))
-app.config["MONGO_URI"] = "mongodb://localhost:27017/cropsDB"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/cropsDB"
+# mongo = PyMongo(app)
 @app.route('/')
 def home():
     return render_template('crop.html')
@@ -16,10 +16,10 @@ def result():
     features = [float(x) for x in request.form.values()]
     prediction = model.predict([features])[0]
     predicted_crop = le.inverse_transform([prediction])[0]
-    mongo.db.crop_info.insert_one({
-        'input': features,
-        'prediction': predicted_crop
-    })
+    # mongo.db.crop_info.insert_one({
+    #     'input': features,
+    #     'prediction': predicted_crop
+    # })
     return render_template('predict.html',prediction = predicted_crop)
 # if __name__ == '__main__':
 #  app.run(debug=True)
